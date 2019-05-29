77d635d5da3be11d2900e46554b082d7312f6530
frostnovus <psolante@deakin.edu.au>
2019-05-29 13:50:44 +1000

    new code added for todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Todos from "./components/Todos";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "take out wife for dinner",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "meeting with boss",
+        completed: false
+      }
+    ]
+  };
+  //toggle complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
 }
 
 export default App;
