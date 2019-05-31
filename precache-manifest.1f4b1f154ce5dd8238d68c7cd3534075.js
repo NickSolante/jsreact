bb5cffac8bde8e131d39a79e8c6e53c3b83120b8
frostnovus <psolante@deakin.edu.au>
2019-05-31 13:12:19 +1000

    Updates

---
+++ precache-manifest.1f4b1f154ce5dd8238d68c7cd3534075.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25972872365052fd811b091b98896ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "326127853d967540c9f5",
+    "url": "/static/css/main.692896d5.chunk.css"
+  },
+  {
+    "revision": "d7c65a71e03fe9ca64ef",
+    "url": "/static/js/2.a0e78736.chunk.js"
+  },
+  {
+    "revision": "326127853d967540c9f5",
+    "url": "/static/js/main.d784e944.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
